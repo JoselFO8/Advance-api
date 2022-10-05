a1d2b9e7d94fab09a882233d9c74c75258effc60
Jose <joseluisfo_@hotmail.com>
2022-10-05 18:39:26 -0500

    solve conflicts in DB_URI conection

--- config/db_mongo.js
+++ config/db_mongo.js
@@ -1,18 +1,10 @@
 require("dotenv").config()
 const mongoose = require('mongoose')
 
-const { MONGO_HOST, MONGO_PASSWORD, MONGO_PORT, MONGO_USER} = process.env
-const DB_URI_PROD = `mongodb://${{ MONGO_USER }}:${{ MONGO_PASSWORD }}@${{ MONGO_HOST }}:${{ MONGO_PORT }}`
-const DB_URI = DB_URI_PROD || process.env.DB_URI_DEV 
-
-
-// mongoose.connect('mongodb://localhost:27017/myapp'); DESDE la documentacion
-// const DB_URI = `mongodb://localhost:27017/advance`
+const DB_URI = process.env.DB_URI_PROD || process.env.DB_URI_DEV 
 
 module.exports = () => {
     const connect = () => {
-        console.log("DESDE db_mongo PROD", process.env.DB_URI_PROD, "MONGOPORT", MONGOPORT);
-        // console.log("DESDE db_mongo DEV", process.env.DB_URI_DEV);
         mongoose.connect(
             DB_URI,
             {
